d39ad08160a7eb1b6ef10004555995dbc6216fd3
o-az <omaraziz.dev@proton.me>
2025-08-14 12:55:49 -0700

    chore: uncomment env var (#686)
    * chore: uncomment env var
    
    * fix: fmt

--- apps/dialog/vite.config.ts
+++ apps/dialog/vite.config.ts
@@ -34,11 +34,9 @@ export default defineConfig(({ mode }) => {
           ? mode === 'test'
             ? '"http://localhost:5173"'
             : '"https://anvil.localhost:5173"'
-          : JSON.stringify('https://service-next.porto.workers.dev'),
-      // TODO(next): uncomment
-      // JSON.stringify(
-      //     env.VITE_WORKERS_URL ?? 'https://service.porto.workers.dev',
-      //   ),
+          : JSON.stringify(
+              env.VITE_WORKERS_URL ?? 'https://service.porto.workers.dev',
+            ),
     },
     plugins: [
       skipMkcert
