f9bba082ed08d9a267e3f53a22bc050cb64bb597
Pierre Bertet <hi@bpier.re>
2025-09-08 23:32:42 +0100

    fix: invalid email button centering (#820)

--- apps/dialog/src/routes/-components/Email.tsx
+++ apps/dialog/src/routes/-components/Email.tsx
@@ -1,5 +1,6 @@
 import { Input } from '@porto/apps/components'
 import { Button, TextButton } from '@porto/ui'
+import { cx } from 'cva'
 import { Hooks } from 'porto/remote'
 import * as React from 'react'
 import * as Dialog from '~/lib/Dialog'
@@ -71,6 +72,8 @@ export function Email(props: Email.Props) {
     )
   }, [actions, cli, hostname])
 
+  const [invalid, setInvalid] = React.useState(false)
+
   return (
     <Layout>
       <Layout.Header className="flex-grow">
@@ -107,6 +110,10 @@ export function Email(props: Email.Props) {
         {actions.includes('sign-up') ? (
           <form
             className="flex w-full flex-grow flex-col gap-2"
+            onInvalid={(event) => {
+              event.preventDefault()
+              setInvalid(true)
+            }}
             onSubmit={onSignUpSubmit}
           >
             {/* If "Sign in" button is present, show the "First time?" text for sign up. */}
@@ -121,10 +128,14 @@ export function Email(props: Email.Props) {
                 Email
               </label>
               <Input
-                className="w-full user-invalid:bg-th_field user-invalid:ring-th_base-negative"
+                className={cx(
+                  'w-full bg-th_field',
+                  invalid && 'not-focus-visible:border-th_negative',
+                )}
                 defaultValue={defaultValue}
                 disabled={status === 'loading' || signingIn}
                 name="email"
+                onChange={() => setInvalid(false)}
                 placeholder="example@ithaca.xyz"
                 type="email"
               />
@@ -140,19 +151,16 @@ export function Email(props: Email.Props) {
               type="submit"
               variant={actions.includes('sign-in') ? 'secondary' : 'primary'}
             >
-              <span className="hidden group-has-[:user-invalid]:block">
-                Invalid email
-              </span>
-              <span className="flex gap-2 group-has-[:user-invalid]:hidden">
-                {actions.includes('sign-in') ? (
-                  'Create Porto account'
-                ) : (
-                  <>
-                    <IconScanFace className="size-5.25" />
-                    Sign up with Porto
-                  </>
-                )}
-              </span>
+              {invalid ? (
+                'Invalid email'
+              ) : actions.includes('sign-in') ? (
+                'Create Porto account'
+              ) : (
+                <div className="flex gap-2">
+                  <IconScanFace className="size-5.25" />
+                  Sign up with Porto
+                </div>
+              )}
             </Button>
           </form>
         ) : (
