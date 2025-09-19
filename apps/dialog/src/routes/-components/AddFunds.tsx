c8858fe5706a83b4baf759ba7e59661623c2ab45
Tom Meagher <tom@meagher.co>
2025-09-19 18:37:56 -0400

    chore(dialog): cross-origin allow payment

--- apps/dialog/src/routes/-components/AddFunds.tsx
+++ apps/dialog/src/routes/-components/AddFunds.tsx
@@ -607,7 +607,7 @@ function Onramp(props: {
     <div>
       {createOrder.isSuccess && createOrder.data?.url && (
         <iframe
-          allow="payment"
+          allow="payment *"
           // TODO: tweak iframe styles
           className={cx(
             'h-12.5 w-full overflow-hidden border-0 bg-transparent',
