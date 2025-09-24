8002b35a67786c9202c193162e0dc8a36fab0c37
jxom <7336481+jxom@users.noreply.github.com>
2025-09-24 15:26:47 +1000

    chore: ignore

--- src/register/index.ts
+++ src/register/index.ts
@@ -3,6 +3,8 @@ import type * as Rpc from '../core/internal/schema/rpc.js'
 
 declare module 'viem' {
   interface Register {
+    // biome-ignore lint/suspicious/noTsIgnore: _
+    // @ts-ignore
     CapabilitiesSchema: {
       getCapabilities: {
         ReturnType: ValueOf<Rpc.wallet_getCapabilities.Response_encoded>
